98c65bca0f9746c14064ea154b4f963e2e52bc4c
Kuncans <akafauna@gmail.com>
2022-01-22 11:54:41 +0000

    Initial Commit

---
+++ AoE 2 Stats/AoE_2_StatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AoE_2_StatsApp.swift
+//  AoE 2 Stats
+//
+//  Created by Duncan Kent on 22/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct AoE_2_StatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
